58948a5009b1c96d9c4c01deac1d62fe2e5445b1
leelaaravind2 <126193303+leelaaravind2@users.noreply.github.com>
2023-06-08 22:00:52 +0530

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,10 +2,10 @@ num = int(input("Enter a number: "))+1
 
 for i in range(0,num):
     if (i%3==0 and i%5==0):
-        print("FizzBuzz",i)
+        print("FizzBuzz")
     elif (i%3==0):
-        print("Fizz   ",i)
+        print("Fizz   ")
     elif (i%5==0):
-        print("Buzz   ",i)
+        print("Buzz   ")
     
 
